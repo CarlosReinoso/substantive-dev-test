433cfc8b46be43b4698439932941c6882bffb746
Carlos Reinoso <carlos.reinoso@myraindrop.co.uk>
2023-10-13 04:04:37 +0100

    add pie chart

--- src/components/SectorsData/PieChartTab.tsx
+++ src/components/SectorsData/PieChartTab.tsx
@@ -1,6 +1,34 @@
-import { IInteractions } from "@/interfaces/sectors";
+import { IInteractions, IPercentage } from "@/interfaces/sectors";
 import { TabPanel } from "@mui/lab";
+import { PieChart, pieArcClasses } from "@mui/x-charts/PieChart";
 
 export default function PieChartTab({ data }: { data: IInteractions[] }) {
-  return <TabPanel value="2">Item 2 </TabPanel>;
+  const updateData = data.map((item: any) => ({
+    label: item.sector,
+    value: item.percentage,
+    id: item.id,
+  }));
+
+  return (
+    <TabPanel value="2">
+      {" "}
+      <PieChart
+        series={[
+          {
+            data: updateData,
+            highlightScope: { faded: "global", highlighted: "item" },
+            faded: { innerRadius: 30, additionalRadius: -30 },
+            outerRadius: 120,
+            
+          },
+        ]}
+        sx={{
+          [`& .${pieArcClasses.faded}`]: {
+            fill: "gray",
+          },
+        }}
+        height={400}
+      />
+    </TabPanel>
+  );
 }
